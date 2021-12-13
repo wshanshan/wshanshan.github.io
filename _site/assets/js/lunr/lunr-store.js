3803bcd266db0bb2da22683d6a0761d6b9c59e7c
shanshan <shanshan@shanshans-MacBook-Air.local>
2021-12-13 15:51:17 -0500

    update jekyll version

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Why can't I find a daycare",
+        "excerpt":"When I started to look for a daycare, I thought it would be a piece of cake. After all, I live in a neighborhood full of young families - Lake View, Chicago. It is 2nd in population among Chicago’s 77 communities. I didn’t ask much about daycares (I thought…). Besides...","categories": ["DataDoesNotLie"],
+        "tags": ["Python","map","geopandas","Chicago","daycare","openData"],
+        "url": "/datadoesnotlie/Chicago-Daycare/",
+        "teaser": null
+      },{
+        "title": "Convert Photos to ASCII Arts with Python",
+        "excerpt":"Printed ASCII art is a fabulous gift for a geeky friend. It’s a stylish decoration that looks great on mugs, t-shirts, and even curtains! Making ASCII art is much easier than it looks, and I’m going to show you how to do it in Python! The great thing about coded...","categories": ["Python"],
+        "tags": ["Python","ASCII","Art","Tutorial"],
+        "url": "/python/asciiart/",
+        "teaser": null
+      }]
