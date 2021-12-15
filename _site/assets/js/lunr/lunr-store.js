16554ab3a1cdc06ea05c032a4cbf597e7986cbb7
shanshan <shanshan@shanshans-MacBook-Air.local>
2021-12-14 22:20:31 -0500

    update configuration. simplify layout

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,13 @@
 var store = [{
-        "title": "Why can't I find a daycare",
-        "excerpt":"When I started to look for a daycare, I thought it would be a piece of cake. After all, I live in a neighborhood full of young families - Lake View, Chicago. It is 2nd in population among Chicago’s 77 communities. I didn’t ask much about daycares (I thought…). Besides...","categories": ["DataDoesNotLie"],
-        "tags": ["Python","map","geopandas","Chicago","daycare","openData"],
-        "url": "/datadoesnotlie/Chicago-Daycare/",
-        "teaser": null
+        "title": "Visualization of Chicago Daycare Data",
+        "excerpt":"When I started to look for a daycare in Chicago around 2016 , I thought it would be a piece of cake (how naive!). After all, I live in a neighborhood full of young families - Lake View, Chicago. It is 2nd in population among Chicago’s 77 communities. I just...","categories": ["OpenData"],
+        "tags": ["Geopandas","OpenData","Geocoding"],
+        "url": "/opendata/Chicago-Daycare/",
+        "teaser": "/assets/images/500x300.png"
       },{
         "title": "Convert Photos to ASCII Arts with Python",
         "excerpt":"Printed ASCII art is a fabulous gift for a geeky friend. The great thing about coded artwork is that you can easily give it your own personal touch! It’s a stylish decoration that looks great on mugs, t-shirts, and even curtains. Making ASCII art is much easier than it looks,...","categories": ["Python"],
-        "tags": ["Python","ASCII","Art","Tutorial"],
+        "tags": ["Python","Art","Tutorial"],
         "url": "/python/asciiart/",
-        "teaser": null
+        "teaser": "/assets/images/500x300.png"
       }]
